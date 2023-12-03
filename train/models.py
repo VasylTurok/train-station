334a4c9edd0271c566975b411c4fe7243bfcb0e0
Vasyl Turok <turokvasil0@gmail.com>
2023-12-03 20:52:21 +0200

    add models

--- train/models.py
+++ train/models.py
@@ -1,3 +1,134 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.full_name
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class TrainType(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Station(models.Model):
+    name = models.CharField(max_length=63)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=63)
+    cargo_num = models.ImageField()
+    places_in_cargo = models.IntegerField()
+    train_type = models.ForeignKey(
+        TrainType,
+        on_delete=models.CASCADE,
+        related_name="trains"
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Station,
+        on_delete=models.CASCADE,
+        related_name="routes"
+    )
+    destination = models.ForeignKey(
+        Station,
+        on_delete=models.CASCADE,
+        related_name="routes"
+    )
+    distance = models.IntegerField()#maybe should do it sach @property
+
+    def __str__(self):
+        return f"{self.source.name}-{self.destination.name}"
+
+
+class Trip(models.Model):
+    route = models.ForeignKey(
+        Route,
+        on_delete=models.CASCADE,
+        related_name="trips"
+    )
+    train = models.ForeignKey(
+        Train,
+        on_delete=models.CASCADE,
+        related_name="trips"
+    )
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    crews = models.ManyToManyField(
+        Crew,
+        blank=True,
+        related_name="trips"
+    )
+
+    def __str__(self):
+        return f"{self.departure_time}-{self.arrival_time}"
+
+
+class Ticket(models.Model):
+    cargo = models.IntegerField()
+    seat = models.IntegerField()
+    trip = models.ForeignKey(
+        Trip,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    def __str__(self):
+        return (
+            f"{str(self.trip)} (row: {self.cargo}, seat: {self.seat})"
+        )
+
+    class Meta:
+        unique_together = ("trip", "row", "seat")
+        ordering = ["cargo", "seat"]
